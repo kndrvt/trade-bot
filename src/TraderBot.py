e17e265c6db52e2597530081b8fb13a66a4eedca
kndrvt <kndrvt@yandex.com>
2021-02-11 21:13:04 +0300

    Init commit

---
+++ src/TraderBot.py
@@ -0,0 +1,7 @@
+"""
+"""
+
+class TraderBot:
+
+    def __init__(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+import sys
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
