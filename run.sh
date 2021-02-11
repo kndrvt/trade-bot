b4cfc482fbf31e7df14614de7b7ef5cd725972b5
kndrvt <kndrvt@yandex.com>
2021-02-11 22:16:17 +0300

    Add configs

---
+++ run.sh
@@ -0,0 +1,1 @@
+poetry run python src/main.py config.yaml
--- src/main.py
+++ src/main.py
@@ -2,11 +2,17 @@
 """
 
 import sys
-
+import yaml
 
 def main(argv):
-    pass
+    config_file = argv[1]
+    with open(config_file) as file:
+        configs = yaml.load(file, Loader=yaml.FullLoader)
+        print(configs)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    if len(sys.argv) > 1:
+        main(sys.argv)
+    else:
+        print("Usage: python main.py <filename path>")
